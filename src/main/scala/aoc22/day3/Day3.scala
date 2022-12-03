0ba72924bcc77591bdb7bf889d65249cfada0320
Joseph Spearritt <joe.spearritt@live.com>
2022-12-03 20:34:23 +1100

    Day 3 solution

--- src/main/scala/aoc22/day3/Day3.scala
+++ src/main/scala/aoc22/day3/Day3.scala
@@ -4,11 +4,15 @@ import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
 
-// Domain models
+case class Rucksack(items: List[Int]):
+  def firstCompartment: List[Int] = items.take(items.size / 2)
+  def secondCompartment: List[Int] = items.takeRight(items.size / 2)
 
-case class Rucksack(items: List[Int])
+  def itemInBoth: Int = firstCompartment.intersect(secondCompartment).head
 
-// Parsing
+  def findBadgeWith(a: Rucksack, b: Rucksack): Int =
+    items.intersect(a.items).intersect(b.items).head
+
 
 object Parsing:
   def charRangeValueParser(start: Char, end: Char): Parser[Int] =
@@ -22,16 +26,16 @@ object Parsing:
   def finalParser: Parser[List[Rucksack]] = CommonParsers.lineSeparated(rucksackParser)
 
 
-// Solution
-
 object Day3 extends SolutionWithParser[List[Rucksack], Int, Int]:
   override def dayNumber: Int = 3
 
   override def parser: Parser[List[Rucksack]] = Parsing.finalParser
 
-  override def solvePart1(input: List[Rucksack]): Int = ???
+  override def solvePart1(input: List[Rucksack]): Int =
+    input.map(_.itemInBoth).sum
 
-  override def solvePart2(input: List[Rucksack]): Int = ???
+  override def solvePart2(input: List[Rucksack]): Int =
+    input.grouped(3).map { case List(a, b, c) => a.findBadgeWith(b, c) }.sum
 
 
 @main def run(): Unit = Day3.run()
