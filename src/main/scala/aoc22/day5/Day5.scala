87fdc1a35e5afc4e3946aa4e7354fa5408128c27
jspearritt <jspearritt@atlassian.com>
2022-12-05 17:10:23 +1100

    Day5 part1

--- src/main/scala/aoc22/day5/Day5.scala
+++ src/main/scala/aoc22/day5/Day5.scala
@@ -7,11 +7,34 @@ case class BlankSpace()
 
 case class Item(id: Char)
 
-case class CraneStack(stackIdx: Int, items: List[Item])
+case class CraneStack(stackIdx: Int, items: List[Item]):
+  def topOfStack: Item = items.head
+
+case class CraneState(stacks: Map[Int, CraneStack]):
+  def applyMove(move: Move): CraneState =
+    val toMove = stacks(move.originStack).items.take(move.quantity).reverse
+    println(s"Moving ${toMove} from ${move.originStack} to ${move.destStack}")
+    CraneState(stacks.map { case (idx, stack) =>
+      if idx == move.originStack then
+        (idx, CraneStack(idx, stack.items.drop(move.quantity)))
+      else if idx == move.destStack then
+        (idx, CraneStack(idx, toMove ++ stack.items))
+      else
+        (idx, stack)
+    })
+
+  def stacksAsList: List[CraneStack] = stacks.values.toList.sortBy(_.stackIdx)
+
+  def getTopsOfStacks: List[Item] = stacksAsList.map(_.topOfStack)
 
 case class Move(quantity: Int, originStack: Int, destStack: Int)
 
-case class Program(startState: Map[Int, CraneStack], moves: List[Move])
+case class Program(startState: CraneState, moves: List[Move]):
+  def run: CraneState =
+    moves.foldLeft(startState) { case (s, m) =>
+      println(s)
+      s.applyMove(m)
+    }
 
 
 object Parsing:
@@ -21,15 +44,15 @@ object Parsing:
     ((Parser.char('[') *> Parser.charIn('A' to 'Z')) <* Parser.char(']')).map(Item.apply)
 
   def itemRowParser: Parser[List[BlankSpace|Item]] =
-    CommonParsers.spaceSeparated(itemParser | blankParser)
+    CommonParsers.separated(itemParser | blankParser, Parser.char(' '))
 
   def stackRangeParser: Parser[Range] =
     CommonParsers.withTrimmedStartingSpaces(CommonParsers.spaceSeparated(CommonParsers.int).map { l => l.min to l.max })
 
-  def craneSectionParser: Parser[Map[Int, CraneStack]] =
+  def craneSectionParser: Parser[CraneState] =
     CommonParsers.pair(CommonParsers.lineSeparated(itemRowParser), stackRangeParser, CommonParsers.newLine)
       .map { case (itemRows, range) =>
-        range.map { index =>
+        val m = range.map { index =>
           // Zero-indexed idx
           val actualIndex = index - range.start
           val filteredItems: List[Item] = itemRows
@@ -38,8 +61,9 @@ object Parsing:
               case BlankSpace() => List()
               case x => List(x.asInstanceOf[Item])
             })
-          (index, CraneStack(index, filteredItems.reverse))
+          (index, CraneStack(index, filteredItems))
         }.toMap
+        CraneState(m)
       }
 
   def moveParser = for
@@ -58,17 +82,22 @@ object Parsing:
   yield Program(startingState, moves)
 
 
-object Day5 extends SolutionWithParser[Program, Int, Int]:
+object Day5 extends SolutionWithParser[Program, String, String]:
   override def dayNumber: Int = 5
 
   override def parser: Parser[Program] = Parsing.programParser
 
-  override def solvePart1(input: Program): Int = ???
+  override def solvePart1(input: Program): String =
+    input.run.getTopsOfStacks.map(_.id).mkString
 
-  override def solvePart2(input: Program): Int = ???
+
+  override def solvePart2(input: Program): String = ???
 
 
 @main def run(): Unit = Day5.run()
 @main def test(): Unit = Day5.test()
 @main def testParser(): Unit =
   Day5.testParser(Parsing.programParser)
+@main def runParser(): Unit =
+  Day5.runParser(Parsing.programParser)
+//  Day5.runParser((Parsing.craneSectionParser <* CommonParsers.blankLine) ~ CommonParsers.lineSeparated(Parsing.moveParser))
