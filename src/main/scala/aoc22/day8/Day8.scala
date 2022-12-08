54b109f2b8601c546b8427ce6728dcdf972ef6e5
jspearritt <jspearritt@atlassian.com>
2022-12-08 16:49:55 +1100

    Day 8 parsing

--- src/main/scala/aoc22/day8/Day8.scala
+++ src/main/scala/aoc22/day8/Day8.scala
@@ -1,21 +1,46 @@
 package aoc22.day8
 
-import aoc22.common.SolutionWithParser
-import cats.parse.Parser
+import aoc22.common.{CommonParsers, SolutionWithParser}
+import cats.parse.{Parser, Rfc5234}
+
+import scala.collection.mutable
+
+
+case class TreeGrid(arr: Array[Array[Int]]):
+  override def toString: String =
+    val sb = mutable.StringBuilder()
+    for row <- arr do
+      for tree <- row do
+        sb.append(tree)
+      sb.append('\n')
+    sb.mkString
 
 
 object Parsing:
-  def finalParser = ???
+  def treeParser: Parser[Int] = Rfc5234.digit.map(_.toInt - '0'.toInt)
+
+  def rowParser: Parser[List[Int]] = treeParser.rep(1).map(_.toList)
+
+  def finalParser: Parser[TreeGrid] = for
+    rows <- CommonParsers.lineSeparated(rowParser)
+  yield
+    val arr: Array[Array[Int]] = Array.ofDim(rows.size, rows.head.size)
+    rows.zipWithIndex.foreach { case (row, rowIdx) =>
+      row.zipWithIndex.foreach { case (digit, colIdx) =>
+        arr(rowIdx)(colIdx) = digit
+      }
+    }
+    TreeGrid(arr)
 
 
-object Day8 extends SolutionWithParser[String, Int, Int]:
+object Day8 extends SolutionWithParser[TreeGrid, Int, Int]:
   override def dayNumber: Int = 8
 
-  override def parser = Parsing.finalParser
+  override def parser: Parser[TreeGrid] = Parsing.finalParser
 
-  override def solvePart1(input: String): Int = ???
+  override def solvePart1(input: TreeGrid): Int = ???
 
-  override def solvePart2(input: String): Int = ???
+  override def solvePart2(input: TreeGrid): Int = ???
 
 
 @main def run(): Unit = Day8.run()
