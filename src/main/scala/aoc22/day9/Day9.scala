5fcad073039334eb7c7f35c0d3167da97ff5d497
jspearritt <jspearritt@atlassian.com>
2022-12-09 17:43:32 +1100

    Day 9 part 1

--- src/main/scala/aoc22/day9/Day9.scala
+++ src/main/scala/aoc22/day9/Day9.scala
@@ -9,8 +9,69 @@ enum Direction(val char: Char):
   case R extends Direction('R')
   case D extends Direction('D')
 
+  def move(pos: (Int, Int)): (Int, Int) =
+    val (x, y) = pos
+    // Coords origin at (0, 0) with (right, down) being positive coords
+    this match {
+      case Direction.L => (x - 1, y)
+      case Direction.U => (x, y - 1)
+      case Direction.R => (x + 1, y)
+      case Direction.D => (x, y + 1)
+    }
 
-case class Instruction(dir: Direction, distance: Int)
+
+def moveMulti(dirs: List[Direction], initial: (Int, Int)): (Int, Int) =
+  dirs.foldLeft(initial) { case (pos, dir) => dir.move(pos) }
+
+
+case class Instruction(dir: Direction, distance: Int):
+  def resolve: List[Direction] = (0 until distance).map(_ => dir).toList
+
+case class State(head: (Int, Int), tail: (Int, Int)):
+  def step(dir: Direction): State =
+    val (tailX, tailY) = tail
+    val (newHeadX, newHeadY) = dir.move(head)
+    val diff = (newHeadX - tailX, newHeadY - tailY)
+    val newTail = diff match {
+      // Tail two to the left
+      case (x, y) if x == 2 && y == 0 => Direction.R.move(tail)
+      // Tail two to the right
+      case (x, y) if x == -2 && y == 0 => Direction.L.move(tail)
+      // Tail two above
+      case (x, y) if x == 0 && y == 2 => Direction.D.move(tail)
+      // Tail two below
+      case (x, y) if x == 0 && y == -2 => Direction.U.move(tail)
+      // Diagonals
+
+      // Left two, above
+      case (x, y) if x == 2 && y == 1 => moveMulti(List(Direction.R, Direction.D), tail)
+      // Left, above two
+      case (x, y) if x == 1 && y == 2 => moveMulti(List(Direction.R, Direction.D), tail)
+
+      // Left two, below
+      case (x, y) if x == 2 && y == -1 => moveMulti(List(Direction.R, Direction.U), tail)
+      // Left, below two
+      case (x, y) if x == 1 && y == -2 => moveMulti(List(Direction.R, Direction.U), tail)
+
+      // Right two, above
+      case (x, y) if x == -2 && y == 1 => moveMulti(List(Direction.L, Direction.D), tail)
+      // Right, above two
+      case (x, y) if x == -1 && y == 2 => moveMulti(List(Direction.L, Direction.D), tail)
+
+      // Right two, below
+      case (x, y) if x == -2 && y == -1 => moveMulti(List(Direction.L, Direction.U), tail)
+      // Right, below two
+      case (x, y) if x == -1 && y == -2 => moveMulti(List(Direction.L, Direction.U), tail)
+
+      // Already in position
+      case (_, _) => tail
+    }
+    State((newHeadX, newHeadY), newTail)
+
+
+
+object State:
+  def initial: State = State((0, 0), (0, 0))
 
 
 object Parsing:
@@ -35,7 +96,15 @@ object Day9 extends SolutionWithParser[List[Instruction], Int, Int]:
 
   override def parser: Parser[List[Instruction]] = Parsing.inputParser
 
-  override def solvePart1(input: List[Instruction]): Int = ???
+  override def solvePart1(input: List[Instruction]): Int =
+    input
+      .flatMap(_.resolve)
+      .foldLeft((Set.empty[State], State.initial)) { case ((prevStates, curState), dir) =>
+        val newState = curState.step(dir)
+        (prevStates + newState, newState)
+      }(0).map(_.tail).toSet.size
+
+
 
   override def solvePart2(input: List[Instruction]): Int = ???
 
