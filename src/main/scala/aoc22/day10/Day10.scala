28b5e0124023b05f25e514430e071055548abb4b
Joseph Spearritt <joe.spearritt@live.com>
2022-12-10 17:23:46 +1100

    Day 10 part 1 more mutably / readably

--- src/main/scala/aoc22/day10/Day10.scala
+++ src/main/scala/aoc22/day10/Day10.scala
@@ -16,67 +16,60 @@ enum Instruction(val requiredCycles: Int):
     }
 
 
-case class ProcessorState(instruction: Instruction, cyclesLeft: Int):
-  def isIdle: Boolean = cyclesLeft <= 0
-
-  def step: ProcessorState =
-    // Bounds check to ensure we don't accidentally continue processing after instr complete
-    if isIdle then
-      throw new Exception(s"Attempted to continue processing after instruction complete: ${this}")
-    ProcessorState(instruction, cyclesLeft - 1)
-
-//  def updateRegister(register: Int): Int =
-//    // Update the register if we have completed the current instruction
-//    if isIdle then
-//      instruction.updateRegister(register)
-//    else
-//      register
+case class InstructionState(instruction: Instruction, cyclesLeft: Int):
+  def isComplete = cyclesLeft <= 0
 
 
-case class SystemState(processor: ProcessorState, cycleNumber: Int, register: Int):
+case class SystemState(currentInstruction: InstructionState, cycleNumber: Int, register: Int):
   def signalStrength: Int = cycleNumber * register
 
-  // Returns the new state, and whether it has started the next instruction
-  def step(nextInstruction: Instruction): (SystemState, Boolean) =
-    // Check if we completed the prior instruction and should therefore take a new one
-    val completedLastInstruction = processor.isIdle
 
-    // Update the register if needed
-    val newRegister = if completedLastInstruction then
-      processor.instruction.updateRegister(register)
-    else
-      register
-
-    // Take the new instruction if required
-    val newProcessor = if completedLastInstruction then
-      ProcessorState(nextInstruction, nextInstruction.requiredCycles)
-    else
-      processor
-
-    // Return the new state, and whether we took an instruction or not
-    (SystemState(newProcessor.step, cycleNumber + 1, newRegister), completedLastInstruction)
+object SystemState:
+  def initial: SystemState = SystemState(InstructionState(Instruction.Noop, 0), 0, 1)
 
 
-case class ProgramState(system: SystemState, instructions: List[Instruction]):
-  def isTerminated: Boolean = instructions.isEmpty
+class CrtSystem(initialState: SystemState, program: List[Instruction]):
+  private var currentState = initialState
+  private val pastStates: MutableMap[Int, SystemState] = MutableMap()
 
-  def step: ProgramState =
+  private var executingProgram = program
+
+  def isTerminated: Boolean = executingProgram.isEmpty
+
+  def getStateAtCycle(cycleNumber: Int): SystemState =
+    pastStates.getOrElse(cycleNumber, currentState)
+
+  private def step(): Unit =
+    // Store this state as a known past state
+    pastStates.addOne((currentState.cycleNumber, currentState))
+    // Do nothing if we have finished our program
     if isTerminated then
-      // If we have completed our instructions do nothing
-      this
-    else
-      val (nextSystem, tookInstruction) = system.step(instructions.head)
-      val nextInstructions = if tookInstruction then
-        instructions.tail
-      else
-        instructions
-      ProgramState(nextSystem, nextInstructions)
+      return ()
 
+    // If we finished our last instruction, update the register and feed a new one in
+    if currentState.currentInstruction.isComplete then
+      val newRegisterValue = currentState.currentInstruction.instruction.updateRegister(currentState.register)
+      // Remove the next instruction from our remaining program
+      val nextInstruction = executingProgram.head
+      executingProgram = executingProgram.tail
+      // Update the state to add the new instruction
+      currentState = SystemState(
+        InstructionState(nextInstruction, nextInstruction.requiredCycles),
+        currentState.cycleNumber,
+        newRegisterValue
+      )
 
-object ProgramState:
-  def initial(instructions: List[Instruction]): ProgramState =
-    // Start with state where cycle number is 1, register is 1, processor has a completed noop (i.e. is idle)
-    ProgramState(SystemState(ProcessorState(Instruction.Noop, 0), 0, 1), instructions)
+    // Step the current instruction (including a newly-started one)
+    val newInstructionState = InstructionState(
+      currentState.currentInstruction.instruction,
+      currentState.currentInstruction.cyclesLeft - 1
+    )
+    currentState = SystemState(newInstructionState, currentState.cycleNumber + 1, currentState.register)
+//    println(currentState)
+
+  def exec(): Unit =
+    while !isTerminated do
+      step()
 
 
 object Parsing:
@@ -99,15 +92,12 @@ object Day10 extends SolutionWithParser[List[Instruction], Int, Int]:
   override def parser: Parser[List[Instruction]] = Parsing.inputParser
 
   override def solvePart1(input: List[Instruction]): Int =
-    val pastStates = MutableMap[Int, ProgramState]()
-    var state = ProgramState.initial(input)
-    pastStates.addOne((state.system.cycleNumber, state))
-    while !state.isTerminated do
-      state = state.step
-      pastStates.addOne((state.system.cycleNumber, state))
+    val system = CrtSystem(SystemState.initial, input)
+    system.exec()
     List(20, 60, 100, 140, 180, 220)
-      .map(pastStates.getOrElse(_, state).system.signalStrength)
+      .map(system.getStateAtCycle(_).signalStrength)
       .sum
+
 
   override def solvePart2(input: List[Instruction]): Int = ???
 
