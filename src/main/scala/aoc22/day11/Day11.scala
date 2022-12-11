abff1a7eb7db9170a7ecb7655c0d393a2f261bad
Joseph Spearritt <joe.spearritt@live.com>
2022-12-11 19:50:56 +1100

    Day 11 part 2

--- src/main/scala/aoc22/day11/Day11.scala
+++ src/main/scala/aoc22/day11/Day11.scala
@@ -4,15 +4,55 @@ import scala.collection.mutable.Map as MutableMap
 import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
+import scala.annotation.targetName
+
+//
+//def primeFactors(n: Int): Set[Int] =
+//  def rec(n: Int, factor: Int): Set[Int] =
+//    // Base case, only need to check up to sqrt(n)
+//    if factor * factor > n then
+//      // n and 1 always factors of themselves
+//      Set(n, 1)
+//    else if n % factor == 0 then
+//      // Factor found
+//      rec(n / factor, factor) + factor
+//    else
+//      // Continue checking factors
+//      rec(n, factor + 1)
+//
+//  // Start at 2 and work up
+//  rec(n, 2)
+//
+//
+//case class WorryLevel(primeFactors: Set[Int]):
+//  def this(initialValue: Int) =
+//    this(primeFactors(initialValue))
+//
+//  def hasFactor(prime: Int): Boolean = primeFactors.contains(prime)
+//
+//  // No way to do addition without prime factoring the new value...
+//  @targetName("add")
+//  def +(other: Int): WorryLevel = ???
+//
+//  @targetName("multiply")
+//  def *(other: Int): WorryLevel = ???
+//
+//  def square: WorryLevel = ???
+//
+//  def moduloFactor(factor: Int): WorryLevel =
+//    if !hasFactor(factor) then
+//      throw Exception("Cannot modulo by non-factor")
+//    WorryLevel(primeFactors - factor)
+
 
 case class MonkeyId(id: Int)
 
 
 enum WorryOperand:
   case Old
-  case Literal(literalValue: Int)
+  case Literal(literalValue: Long)
 
-  def value(oldValue: Int): Int = this match {
+  def value(oldValue: Long): Long = this match {
     case Old => oldValue
     case Literal(v) => v
   }
@@ -24,31 +64,31 @@ enum WorryOperator:
 
 
 case class WorryOperation(left: WorryOperand, right: WorryOperand, op: WorryOperator):
-  def apply(old: Int): Int = op match {
+  def apply(old: Long): Long = op match {
     case WorryOperator.Add => left.value(old) + right.value(old)
     case WorryOperator.Multiply => left.value(old) * right.value(old)
   }
 
 
 case class WorryTest(factor: Int, ifTrueId: MonkeyId, ifFalseId: MonkeyId):
-  def moduloFactor(worryLevel: Int): Int = worryLevel % factor
+  def moduloFactor(worryLevel: Long): Long = worryLevel % factor
 
-  def apply(worryLevel: Int): MonkeyId =
+  def apply(worryLevel: Long): MonkeyId =
     if moduloFactor(worryLevel) == 0 then
       ifTrueId
     else
       ifFalseId
 
 
-case class Monkey(id: MonkeyId, items: List[Int], op: WorryOperation, test: WorryTest):
+case class Monkey(id: MonkeyId, items: List[Long], op: WorryOperation, test: WorryTest):
   // Apply the operator
-  private def inspect(item: Int): Int = op(item)
+  private def inspect(item: Long): Long = op(item)
 
   // Int-divide by 3 after completing inspection
-  private def decreaseWorry(item: Int): Int = item / 3
+  private def decreaseWorry(item: Long): Long = item / 3
 
   // Inspect all items and determine where they should be thrown
-  def performInspections(decreaseWorryAfterInspection: Boolean): List[(Int, MonkeyId)] =
+  def performInspections(decreaseWorryAfterInspection: Boolean): List[(Long, MonkeyId)] =
     items.map { worryLevel =>
       val inspected = inspect(worryLevel)
       val finalised = if decreaseWorryAfterInspection then
@@ -64,7 +104,7 @@ case class Monkey(id: MonkeyId, items: List[Int], op: WorryOperation, test: Worr
     Monkey(id, List(), op, test)
 
   // Take items thrown to this monkey and append them to our list
-  def acceptItems(incomingItems: Iterable[Int]): Monkey =
+  def acceptItems(incomingItems: Iterable[Long]): Monkey =
     Monkey(id, items ++ incomingItems.toList, op, test)
 
 
@@ -72,7 +112,16 @@ class MonkeyGroup(initialState: Map[MonkeyId, Monkey], decreaseWorryAfterInspect
   // State of the monkeys / holding items
   private val state = MutableMap.from(initialState)
   // To start with no monkey has performed inspections
-  private val inspectionsPerformed = MutableMap.from(initialState.view.mapValues(_ => 0))
+  private val inspectionsPerformed = MutableMap.from(initialState.view.mapValues(_ => 0L))
+
+  // In part 1 we don't need this
+  // In part 2 we modulo by the product of all the divisor tests
+  // to keep from overflowing
+  private val overallModuloFactor =
+    if decreaseWorryAfterInspections then
+      None
+    else
+      Some(initialState.values.map(_.test.factor).product)
 
   // The actual number of monkeys doesn't change
   private def sortedMonkeyIds = initialState.keys.toList.sortBy(_.id)
@@ -88,7 +137,9 @@ class MonkeyGroup(initialState: Map[MonkeyId, Monkey], decreaseWorryAfterInspect
     // Other monkeys should then receive the thrown items
     inspectedAndThrown.groupBy { case (_, id) => id }
       .foreach { case (receivingMonkey, items) =>
-        state(receivingMonkey) = state(receivingMonkey).acceptItems(items.map(_(0)))
+        state(receivingMonkey) = state(receivingMonkey).acceptItems(items.map { case (item, _) =>
+          overallModuloFactor.map(item % _).getOrElse(item)
+        })
       }
 
   def runRound(): Unit =
@@ -96,7 +147,7 @@ class MonkeyGroup(initialState: Map[MonkeyId, Monkey], decreaseWorryAfterInspect
     sortedMonkeyIds.foreach(id => giveTurn(id))
 
   // Monkey business defined by the product of the number of inspections performed by the top-inspecting two monkeys
-  def monkeyBusiness: Int =
+  def monkeyBusiness: Long =
     inspectionsPerformed.values.toList.sorted.takeRight(2).product
 
   def printState: String = state.toList.sortBy(_(0).id).map { case (id, monkey) =>
@@ -118,11 +169,11 @@ object Parsing:
   yield
     id
 
-  def startingItemsParser: Parser[List[Int]] = for
+  def startingItemsParser: Parser[List[Long]] = for
     _ <- Parser.string("Starting items: ")
     items <- CommonParsers.separated(CommonParsers.int, Parser.string(", "))
   yield
-    items
+    items.map(_.toLong)
 
   def operandParser: Parser[WorryOperand] =
     Parser.string("old").map(_ => WorryOperand.Old) | CommonParsers.int.map(WorryOperand.Literal(_))
@@ -163,12 +214,12 @@ object Parsing:
     }
 
 
-object Day11 extends SolutionWithParser[Map[MonkeyId, Monkey], Int, Int]:
+object Day11 extends SolutionWithParser[Map[MonkeyId, Monkey], Long, Long]:
   override def dayNumber: Int = 11
 
   override def parser: Parser[Map[MonkeyId, Monkey]] = Parsing.inputParser
 
-  override def solvePart1(input: Map[MonkeyId, Monkey]): Int =
+  override def solvePart1(input: Map[MonkeyId, Monkey]): Long =
     val group = MonkeyGroup(input, true)
     (1 to 20).foreach { r =>
       group.runRound()
@@ -180,12 +231,13 @@ object Day11 extends SolutionWithParser[Map[MonkeyId, Monkey], Int, Int]:
     println()
     group.monkeyBusiness
 
-  override def solvePart2(input: Map[MonkeyId, Monkey]): Int =
-    ???
+  override def solvePart2(input: Map[MonkeyId, Monkey]): Long =
     val group = MonkeyGroup(input, false)
     (1 to 10000).foreach { r =>
       group.runRound()
     }
+    println()
+    println(group.printState)
     println()
     println(group.printInspectionsState)
     println()
