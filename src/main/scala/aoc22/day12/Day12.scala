5d62f7b19a47de9d5ecf7e4976ae20bc4dafcc50
jspearritt <jspearritt@atlassian.com>
2022-12-12 17:03:04 +1100

    Day 12 parsing

--- src/main/scala/aoc22/day12/Day12.scala
+++ src/main/scala/aoc22/day12/Day12.scala
@@ -1,24 +1,58 @@
 package aoc22.day12
 
-import aoc22.common.SolutionWithParser
+import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
 
+def heightFromChar(char: Char): Int = char - 'a'
+def heightToChar(height: Int): Char = ('a' + height).toChar
 
+
+case class Terrain(terrain: Array[Array[Int]], start: (Int, Int), end: (Int, Int))
 
 
 object Parsing:
-  def inputParser = ???
+  enum LocationParseValue(val value: Int):
+    case StartLocation extends LocationParseValue(heightFromChar('a'))
+    case EndLocation extends LocationParseValue(heightFromChar('z'))
+    case Location(c: Char) extends LocationParseValue(heightFromChar(c))
+
+  def startLoc: Parser[LocationParseValue] =
+    Parser.char('S').map(_ => LocationParseValue.StartLocation)
+
+  def endLoc: Parser[LocationParseValue] =
+    Parser.char('E').map(_ => LocationParseValue.EndLocation)
+
+  def regularLoc: Parser[LocationParseValue] =
+    Parser.charIn('a' to 'z').map(c => LocationParseValue.Location(c))
+
+  def loc: Parser[LocationParseValue] = startLoc | endLoc | regularLoc
+
+  def locArray: Parser[Array[Array[LocationParseValue]]] =
+    CommonParsers.lineSeparated(loc.rep(1).map(l => Array.from(l.toList))).map(r => Array.from(r))
+
+  def inputParser: Parser[Terrain] = locArray.map { arr =>
+    var start = (0, 0)
+    var end = (0, 0)
+    for (row, rowIdx) <- arr.zipWithIndex do
+      for (l, colIdx) <- row.zipWithIndex do
+        l match
+          case LocationParseValue.StartLocation => start = (rowIdx, colIdx)
+          case LocationParseValue.EndLocation => end = (rowIdx, colIdx)
+          case _ => ()
+    val finalTerrain = arr.map(row => row.map(loc => loc.value))
+    Terrain(finalTerrain, start, end)
+  }
 
 
-object Day12 extends SolutionWithParser[String, Int, Int]:
+object Day12 extends SolutionWithParser[Terrain, Int, Int]:
   override def dayNumber: Int = 12
 
-  override def parser: Parser[String] = Parsing.inputParser
+  override def parser: Parser[Terrain] = Parsing.inputParser
 
-  override def solvePart1(input: String): Int = ???
+  override def solvePart1(input: Terrain): Int = ???
 
-  override def solvePart2(input: String): Int = ???
+  override def solvePart2(input: Terrain): Int = ???
 
 
 @main def run(): Unit = Day12.run()
