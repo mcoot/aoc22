fa779c65137a6c863bcc8a5762891872f41f2e35
Joseph Spearritt <joe.spearritt@live.com>
2022-12-17 13:57:46 +1100

    Day 16 basic graph setup

--- src/main/scala/aoc22/day16/Day16.scala
+++ src/main/scala/aoc22/day16/Day16.scala
@@ -3,12 +3,69 @@ package aoc22.day16
 import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
+
+val TIME_MAX = 30
+
 case class ValveId(name: String)
 
 case class Valve(id: ValveId, flowRate: Int, neighbours: Set[ValveId])
 
 case class Network(definitions: List[Valve]):
-  val valves: Map[ValveId, Valve] = Map.from(definitions.map(v => (v.id, v)))
+  val vertices: Map[ValveId, Valve] = Map.from(definitions.map(v => (v.id, v)))
+  val edges: Set[(ValveId, ValveId)] = definitions.flatMap(v => v.neighbours.map((v.id, _))).toSet
+
+  // Pre-calculate paths from every valve to every other
+  // This is always possible as in the input there are always reverse tunnels for every given tunnel
+  val paths: Map[(ValveId, ValveId), List[ValveId]] = buildAllPairsShortestPaths
+
+  // Calculating all-pairs shortest paths via Floyd-Warshall
+  private def buildAllPairsShortestPaths: Map[(ValveId, ValveId), List[ValveId]] =
+    // Map array indices back to the ValveId
+    val indices: Map[ValveId, Int] = Map.from(definitions.map(_.id).zipWithIndex)
+    // No initial paths
+    val paths: Array[Array[Option[List[ValveId]]]] = Array.fill(indices.size, indices.size)(None)
+
+    def curDist(from: ValveId, to: ValveId): Int =
+      // Not using Int.MaxValue because of overflow, but confident we will never have 100k length paths
+      // based on the size of the input graph
+      paths(indices(from))(indices(to)).map(_.size).getOrElse(100000)
+
+    // Distance to self is 0
+    for v <- vertices.keys do
+      paths(indices(v))(indices(v)) = Some(List())
+
+    // Direct edges have distance 1
+    for (u, v) <- edges do
+      paths(indices(u))(indices(v)) = Some(List(v))
+
+    // Apply Floyd-Warshall to calculate the shortest distances
+    for
+      k <- vertices.keys
+      i <- vertices.keys
+      j <- vertices.keys
+    do
+      if curDist(i, j) > curDist(i, k) + curDist(k, j) then
+        // For the latter path to ever be better we must have already found those routes
+        // I.e. they can't be None
+        paths(indices(i))(indices(j)) = Some(paths(indices(i))(indices(k)).get ++ paths(indices(k))(indices(j)).get)
+
+    Map.from(
+      for
+        i <- vertices.keys
+        j <- vertices.keys
+      yield
+        // Confident here we do in fact have paths, graph is connected
+        ((i, j), paths(indices(i))(indices(j)).get)
+    )
+
+  def apply(id: ValveId): Valve = vertices(id)
+
+case class NetworkState(network: Network, openValves: Set[ValveId], currentlyAt: ValveId):
+  def totalFlowPerMinute: Int =
+    openValves.map(network(_).flowRate).sum
+
+  def timeToMoveTo(valveId: ValveId): Int =
+    ???
 
 
 object Parsing:
@@ -28,7 +85,7 @@ object Parsing:
       Valve(id, flowRate, neighbours.toSet)
 
   def inputParser: Parser[Network] =
-    CommonParsers.lineSeparated(valve).map(Network.apply)
+    CommonParsers.lineSeparated(valve).map(Network(_))
 
 
 object Day16 extends SolutionWithParser[Network, Int, Int]:
@@ -36,9 +93,11 @@ object Day16 extends SolutionWithParser[Network, Int, Int]:
 
   override def parser: Parser[Network] = Parsing.inputParser
 
-  override def solvePart1(input: Network): Int = ???
+  override def solvePart1(input: Network): Int =
+    ???
 
-  override def solvePart2(input: Network): Int = ???
+  override def solvePart2(input: Network): Int =
+    ???
 
 
 
