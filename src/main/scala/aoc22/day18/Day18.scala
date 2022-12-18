78bc9e2fc79344751836a8459c91eec557c6deaf
Joseph Spearritt <joe.spearritt@live.com>
2022-12-18 18:30:03 +1100

    Day 18 part 2

--- src/main/scala/aoc22/day18/Day18.scala
+++ src/main/scala/aoc22/day18/Day18.scala
@@ -1,6 +1,6 @@
 package aoc22.day18
 
-import scala.collection.mutable.Set as MutableSet
+import scala.collection.mutable.{Set as MutableSet, Map as MutableMap}
 import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
@@ -8,41 +8,89 @@ import cats.parse.Parser
 case class Cube(x: Int, y: Int, z: Int):
   def this(t: (Int, Int, Int)) = this(t(0), t(1), t(2))
 
-  def maximalAdjacency: CubeInDroplet = CubeInDroplet(this, List(
-    Some(Cube(x - 1, y, z)),
-    Some(Cube(x + 1, y, z)),
-    Some(Cube(x, y - 1, z)),
-    Some(Cube(x, y + 1, z)),
-    Some(Cube(x, y, z - 1)),
-    Some(Cube(x, y, z + 1)),
-  ))
-
-  def adjacentPositions: List[Cube] =
-    maximalAdjacency.neighbours.map(_.get)
-
-  def isAdjacentTo(other: Cube): Boolean = adjacentPositions.contains(other)
-
-
-// List always of size 6
-// easier than using a tuple
-case class CubeInDroplet(cube: Cube, neighbours: List[Option[Cube]])
+  def adjacentPositions: List[Cube] = List(
+    Cube(x - 1, y, z),
+    Cube(x + 1, y, z),
+    Cube(x, y - 1, z),
+    Cube(x, y + 1, z),
+    Cube(x, y, z - 1),
+    Cube(x, y, z + 1)
+  )
 
 
 case class Droplet(cubes: List[Cube]):
   val cubeSet: Set[Cube] = cubes.toSet
 
-  def cubeGraph: Map[Cube, CubeInDroplet] =
-    cubes.map { cube =>
-      (cube, CubeInDroplet(cube, cube.maximalAdjacency.neighbours.map { n =>
-        Some(n.get).filter(cubeSet.contains)
-      }))
-    }.toMap
+  def cubeExists(cube: Cube): Boolean = cubeSet.contains(cube)
 
-  def totalSurfaceArea: Int =
-    cubeGraph.values.map(c => c.neighbours.count(_.isEmpty)).sum
+  def minBound: Cube =
+    Cube(cubeSet.map(_.x).min, cubeSet.map(_.y).min, cubeSet.map(_.z).min)
+
+  def maxBound: Cube =
+    Cube(cubeSet.map(_.x).max, cubeSet.map(_.y).max, cubeSet.map(_.z).max)
+
+  def isOnBoundary(cube: Cube): Boolean =
+    cube.adjacentPositions.exists { n =>
+      n.x < minBound.x || n.y < minBound.y || n.z < minBound.z
+        || n.x > maxBound.x || n.y > maxBound.y || n.z > maxBound.z
+    }
+
+  def surfaceAreaOf(cs: Set[Cube], existing: Boolean): Int =
+    cs
+      // toList because other map() will secretly dedupe
+      .toList
+      .map { c => c.adjacentPositions.count(n => (existing && !cubeExists(n)) || (!existing && cubeExists(n))) }
+      .sum
+
+  def totalSurfaceArea: Int = surfaceAreaOf(cubeSet, true)
+
+  // Depth-first search of a potential hole
+  // Returns the cube positions making up the whole if it is truly internal
+  // (i.e. if a boundary is not encountered)
+  def dfsHole(pos: Cube, visited: Set[Cube], cache: MutableMap[Cube, Option[Set[Cube]]]): Option[Set[Cube]] =
+    if cache.contains(pos) then
+      return cache(pos)
+
+    // Base case, our current position hits a boundary, then we aren't in a hole
+    if isOnBoundary(pos) then
+      cache(pos) = None
+      return None
+
+    // Find new empty spaces to go
+    val toVisit = pos.adjacentPositions.filter(n => !visited.contains(n) && !cubeExists(n))
+
+    var newVisited = visited + pos
+
+    for n <- toVisit do
+      dfsHole(n, newVisited, cache) match
+        case Some(value) =>
+          newVisited ++= value
+        case None =>
+          // Immediately propagate a boundary up, no point continuing
+          cache(pos) = None
+          return None
+    cache(pos) = Some(newVisited)
+    Some(newVisited)
+
+  def holes: Set[Set[Cube]] =
+    // Start searching from empty adjacent positions of filled ones
+    // (Dedupes due to being a set)
+    val emptyAdjacencies = cubeSet.flatMap(c => c.adjacentPositions.filter(!cubeExists(_))).toList
+
+    var holes: Set[Set[Cube]] = Set.empty
+
+    for (c, idx) <- emptyAdjacencies.zipWithIndex do
+
+      if !holes.exists(_.contains(c)) then
+        val res = dfsHole(c, Set.empty, MutableMap.empty)
+        if res.isDefined then
+          holes = holes + res.get
+
+    holes
+
 
   def externalSurfaceArea: Int =
-    cubeGraph.values.map(c => c.neighbours.count(_.isEmpty)).sum
+    totalSurfaceArea - holes.toList.map(h => surfaceAreaOf(h, false)).sum
 
 
 object Day18 extends SolutionWithParser[List[Cube], Int, Int]:
