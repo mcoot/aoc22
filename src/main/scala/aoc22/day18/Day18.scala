d49b2e70ec9116922015115d73e5333c1499c0e9
Joseph Spearritt <joe.spearritt@live.com>
2022-12-18 16:31:35 +1100

    Day 18 part 1

--- src/main/scala/aoc22/day18/Day18.scala
+++ src/main/scala/aoc22/day18/Day18.scala
@@ -1,14 +1,37 @@
 package aoc22.day18
 
+import scala.collection.mutable.ArrayBuffer
 import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
+
 
 case class Cube(x: Int, y: Int, z: Int):
   def this(t: (Int, Int, Int)) = this(t(0), t(1), t(2))
 
+  def adjacencies: List[Cube] = List(
+    Cube(x - 1, y, z),
+    Cube(x + 1, y, z),
+    Cube(x, y - 1, z),
+    Cube(x, y + 1, z),
+    Cube(x, y, z - 1),
+    Cube(x, y, z + 1),
+  )
+
+  def isAdjacentTo(other: Cube): Boolean = adjacencies.contains(other)
+
+
+def findSurfaceArea(cubes: List[Cube]): Int =
+  val cubeCartesian =
+    for
+      a <- cubes
+      b <- cubes
+    yield (a, b)
+  cubes.size * 6 - cubeCartesian.count { case (a, b) => a.isAdjacentTo(b) }
+
+
+
 object Day18 extends SolutionWithParser[List[Cube], Int, Int]:
   override def dayNumber: Int = 18
-
 
   override def parser: Parser[List[Cube]] =
     CommonParsers.lineSeparated(
@@ -16,7 +39,7 @@ object Day18 extends SolutionWithParser[List[Cube], Int, Int]:
         .map(Cube.apply)
     )
 
-  override def solvePart1(input: List[Cube]): Int = ???
+  override def solvePart1(input: List[Cube]): Int = findSurfaceArea(input)
 
   override def solvePart2(input: List[Cube]): Int = ???
 
