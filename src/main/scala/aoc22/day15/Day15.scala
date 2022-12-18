fdd81fb5248cec9690b66a6c389340c017707f4f
Joseph Spearritt <joe.spearritt@live.com>
2022-12-18 22:55:43 +1100

    Day 15 part 1

--- src/main/scala/aoc22/day15/Day15.scala
+++ src/main/scala/aoc22/day15/Day15.scala
@@ -1,5 +1,6 @@
 package aoc22.day15
 
+import scala.collection.mutable.Set as MutableSet
 import aoc22.common.{CommonParsers, SolutionWithParser}
 import cats.parse.Parser
 
@@ -8,12 +9,59 @@ case class Position(x: Int, y: Int):
   def this(p: (Int, Int)) = this(p(0), p(1))
   def pair: (Int, Int) = (x, y)
 
+  def hdist(otherX: Int): Int = Math.abs(otherX - x)
+  def hdist(other: Position): Int = hdist(other.x)
+
+  def vdist(otherY: Int): Int = Math.abs(otherY - y)
+  def vdist(other: Position): Int = vdist(other.y)
+
+  def manhattan(other: Position): Int = hdist(other) + vdist(other)
+
+  def tuningFrequency: Int = x * y
+
 
 extension (p: (Int, Int))
   def pos: Position = Position(p(0), p(1))
 
 
-case class Sensor(pos: Position, nearestBeacon: Position)
+case class Sensor(pos: Position, nearestBeacon: Position):
+  def distanceToNearest: Int = pos.manhattan(nearestBeacon)
+
+  def inRangeOfRow(rowY: Int): Boolean = pos.vdist(rowY) <= distanceToNearest
+
+  def positionsExcludedInRow(rowY: Int): Set[Position] =
+    if !inRangeOfRow(rowY) then
+      return Set.empty
+
+    val possibleHorDist = distanceToNearest - pos.vdist(rowY)
+    val (minX, maxX) = (pos.x - possibleHorDist, pos.x + possibleHorDist)
+    (minX to maxX)
+      .map(x => (x, rowY).pos)
+      .filter(_ != nearestBeacon)
+      .toSet
+
+  def positionsExcluded: MutableSet[Position] =
+    val d = distanceToNearest
+    val excluded: MutableSet[Position] = MutableSet.empty
+    for
+      x <- pos.x - d to pos.x + d
+      y <- pos.y - d to pos.y + d
+      if pos.manhattan((x, y).pos) <= d && (x, y).pos != nearestBeacon
+    do
+      excluded.addOne((x, y).pos)
+    excluded
+
+
+def findPositionsInRowWhichCannotHaveBeacons(sensors: List[Sensor], rowY: Int): Set[Position] =
+  // Ignore sensors far enough from this row that their sensor zones do not overlap it
+  val relevantSensors = sensors.filter(s => s.inRangeOfRow(rowY))
+  println(s"There were ${relevantSensors.size} relevant sensors")
+
+  val excluded: MutableSet[Position] = MutableSet.empty
+  for (sensor, i) <- relevantSensors.zipWithIndex do
+    println(s"Handling sensor ${i}")
+    excluded.addAll(sensor.positionsExcludedInRow(rowY))
+  excluded.toSet
 
 
 object Day15 extends SolutionWithParser[List[Sensor], Int, Int]:
@@ -40,7 +88,8 @@ object Day15 extends SolutionWithParser[List[Sensor], Int, Int]:
   override def parser: Parser[List[Sensor]] =
     CommonParsers.lineSeparated(sensorParser)
 
-  override def solvePart1(input: List[Sensor]): Int = ???
+  override def solvePart1(input: List[Sensor]): Int =
+    findPositionsInRowWhichCannotHaveBeacons(input, 2000000).size
 
   override def solvePart2(input: List[Sensor]): Int = ???
 
