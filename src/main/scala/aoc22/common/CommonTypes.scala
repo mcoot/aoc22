da12444b3672b8ae407c497233727995c0b63161
jspearritt <jspearritt@atlassian.com>
2022-12-31 15:25:49 +1000

    Day 22 part 1

--- src/main/scala/aoc22/common/CommonTypes.scala
+++ src/main/scala/aoc22/common/CommonTypes.scala
@@ -14,9 +14,12 @@ case class Point2D(x: Int, y: Int):
   // Convert to pair
   def pair: (Int, Int) = (x, y)
 
-  // Vector / complex addition
+  // Vector / complex addition and subtraction
   @targetName("add")
   def +(other: Point2D): Point2D = Point2D(x + other.x, y + other.y)
+
+  @targetName("subtract")
+  def -(other: Point2D): Point2D = Point2D(x - other.x, y - other.y)
 
   // Scalar multiplication
   @targetName("mult")
@@ -31,8 +34,18 @@ case class Point2D(x: Int, y: Int):
 
   def manhattan(other: Point2D): Int = hdist(other) + vdist(other)
 
+  def magnitude: Double = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))
+
   // Check if in bounds
   def inBounds(minBound: Point2D, maxBound: Point2D): Boolean =
     val Point2D(minX, minY) = minBound
     val Point2D(maxX, maxY) = maxBound
     x >= minX && x <= maxX && y >= minY && y <= maxY
+
+  def constrainToBounds(minBound: Point2D, maxBound: Point2D): Point2D =
+    Point2D(Math.max(minBound.x, Math.min(maxBound.x, x)), Math.max(minBound.y, Math.min(maxBound.y, y)))
+
+  // Modulo the components independently, useful for wrapping
+  // named floorMod rather than % because it does sane (Math.floorMod) handling of negatives
+  def floorMod(modulus: Point2D): Point2D =
+    Point2D(Math.floorMod(x, modulus.x), Math.floorMod(y, modulus.y))
--- src/main/scala/aoc22/day22/Day22.scala
+++ src/main/scala/aoc22/day22/Day22.scala
@@ -1,7 +1,9 @@
 package aoc22.day22
 
-import aoc22.common.{CommonParsers, Point2D, pos, SolutionWithParser}
+import aoc22.common.{CommonParsers, Point2D, SolutionWithParser, pos}
 import cats.parse.Parser
+
+import scala.annotation.tailrec
 
 
 enum TurnDirection:
@@ -14,6 +16,15 @@ enum Direction:
   case Right
   case Up
   case Down
+
+  def moveVec: Point2D = this match
+    case Direction.Left => (-1, 0).pos
+    case Direction.Right => (1, 0).pos
+    case Direction.Up => (0, -1).pos
+    case Direction.Down => (0, 1).pos
+
+  def isHor: Boolean = Direction.horDirections.contains(this)
+  def isVert: Boolean = Direction.vertDirections.contains(this)
 
   def flip: Direction = this match
     case Direction.Left => Direction.Right
@@ -40,6 +51,12 @@ enum Direction:
     case Direction.Up => 3
 
 
+object Direction:
+  def horDirections: List[Direction] = List(Direction.Left, Direction.Right)
+  def vertDirections: List[Direction] = List(Direction.Up, Direction.Down)
+  def directions: List[Direction] = horDirections ++ vertDirections
+
+
 enum Instruction:
   case Forward(steps: Int)
   case Turn(dir: TurnDirection)
@@ -55,20 +72,36 @@ case class Plane(id: Int, posInNet: Point2D, arr: Array[Array[Boolean]]):
 
   def maxBound: Point2D = (arr.head.length - 1, arr.length - 1).pos
 
+  def isWall(pos: Point2D): Boolean = arr(pos.y)(pos.x)
+
 
 // Raw grid data as a a 2d grid of square segments
-case class RawGridData(arr: Array[Array[Option[Plane]]])
+case class PlaneGrid(arr: Array[Array[Option[Plane]]]):
+  val planes: Map[Int, Plane] = arr
+    .flatMap(row => row.collect { case Some(p) => p } )
+    .map(p => (p.id, p))
+    .toMap
+
+  val startingPlane: Plane = planes.values.filter(_.posInNet.y == 0).minBy(_.posInNet.x)
+
+  val gridMaxBound: Point2D = (arr.head.length - 1, arr.length - 1).pos
+
+  def apply(gridCoord: Point2D): Option[Plane] = arr(gridCoord.y)(gridCoord.x)
 
 
 // Describes where the player is by the plane they're on, position in the plane and cur direction
 case class PlayerPos(plane: Plane, pos: Point2D, dir: Direction):
+  def turn(td: TurnDirection): PlayerPos = copy(dir = dir.turn(td))
+
+  def step: PlayerPos = copy(pos = pos + dir.moveVec)
+
   def score: Long =
     val netPos = plane.netPositionFor(pos)
     (netPos.y + 1) * 1000 + (netPos.x + 1) * 4 + dir.facingVal
 
 
 // When moving across planes, a coordinate may change in one of several ways\
-enum EdgeCoordTransition:
+enum CoordTransition:
   // The coordinate could wrap to 0
   case ToZero
   // The coordinate could wrap to the max value for that plane
@@ -79,35 +112,97 @@ enum EdgeCoordTransition:
   case Swapped
 
   def applyToCoordinate(coord: Int, otherCoord: Int, max: Int): Int = this match
-    case EdgeCoordTransition.ToZero => 0
-    case EdgeCoordTransition.ToMax => max
-    case EdgeCoordTransition.Maintained => coord
-    case EdgeCoordTransition.Swapped => otherCoord
+    case CoordTransition.ToZero => 0
+    case CoordTransition.ToMax => max
+    case CoordTransition.Maintained => coord
+    case CoordTransition.Swapped => otherCoord
 
 // Describes how the player's position is mapped when moving from one plane to another
 // Theory: the direction the player will be facing can be inferred from their existing direction
 // and the position translation
 // TODO: I NEED A CUBE
-case class EdgeTransition(x: EdgeCoordTransition, y: EdgeCoordTransition):
-  def applyToPos(pos: Point2D, maxBound: Point2D): Point2D =
-    (x.applyToCoordinate(pos.x, pos.y, maxBound.x), y.applyToCoordinate(pos.y, pos.x, maxBound.y)).pos
+case class EdgeTransition(toPlane: Plane, x: CoordTransition, y: CoordTransition, newDir: Direction):
+  protected def applyToPos(pos: Point2D): Point2D =
+    (x.applyToCoordinate(pos.x, pos.y, toPlane.maxBound.x), y.applyToCoordinate(pos.y, pos.x, toPlane.maxBound.y)).pos
 
-  def applyToDirection(dir: Direction): Direction = (x, y) match
-    case (EdgeCoordTransition.Maintained, EdgeCoordTransition.ToZero) => dir
-    case _ => throw Exception("No direction mapping for edge transition")
+  // Take one step over the edge...
+  def apply(pos: PlayerPos): PlayerPos =
+    PlayerPos(toPlane, applyToPos(pos.pos), newDir)
 
 
-// Maps the player position from a given plane id to another
-case class EdgeMapping(fromPlane: Int, toPlane: Int, transition: EdgeTransition):
-  def apply(p: PlayerPos): PlayerPos = ???
+//    (x, y, dir) match
+//      // top to front edge, x value maintained on the new plane, y becomes 0, direction remains down
+//      case (CoordTransition.Maintained, CoordTransition.ToZero, Direction.Down) => Direction.Down
+//      // top to back edge, x value maintained, y becomes max, direction remains up
+//      case (CoordTransition.Maintained, CoordTransition.ToMax, Direction.Up) => Direction.Up
+//      // top to right edge, x value becomes y from the prev plane, y becomes 0, direction becomes down
+//      case (CoordTransition.Swapped, CoordTransition.ToZero, Direction.Right) => Direction.Down
+//      // top to left edge, x value becomes y from the prev plane, y becomes 0, direction becomes down
+//      case (CoordTransition.Swapped, CoordTransition.ToZero, Direction.Left) => Direction.Down
+//      case _ => throw Exception("No direction mapping for edge transition")
+
+
+// For part 1 we just need to consider wrapping transitions on a 2D plane
+def part1TransitionFor(planeGrid: PlaneGrid, fromPlane: Plane, dir: Direction): EdgeTransition =
+  // Move along the direction, potentially wrapping
+  var movedTo = (fromPlane.posInNet + dir.moveVec).floorMod(planeGrid.gridMaxBound + (1, 1).pos)
+  while planeGrid(movedTo).isEmpty do
+    movedTo = (movedTo + dir.moveVec).floorMod(planeGrid.gridMaxBound + (1, 1).pos)
+
+  val toPlane = planeGrid(movedTo).get
+  dir match
+    case Direction.Left => EdgeTransition(toPlane, CoordTransition.ToMax, CoordTransition.Maintained, Direction.Left)
+    case Direction.Right => EdgeTransition(toPlane, CoordTransition.ToZero, CoordTransition.Maintained, Direction.Right)
+    case Direction.Up => EdgeTransition(toPlane, CoordTransition.Maintained, CoordTransition.ToMax, Direction.Up)
+    case Direction.Down => EdgeTransition(toPlane, CoordTransition.Maintained, CoordTransition.ToZero, Direction.Down)
 
 
 // A set of planes with information about how to map between them
-case class GroveMap(planeList: List[Plane], edgeMappings: List[EdgeMapping]):
-  val planes: Map[Int, Plane] = planeList.map(p => (p.id, p)).toMap
+case class GroveMapPt1(grid: PlaneGrid):
+  val initialPosition: PlayerPos = PlayerPos(grid.startingPlane, (0, 0).pos, Direction.Right)
+
+  // Attempt to step forward, failing if blocked by a wall
+  private def applyStep(priorPos: PlayerPos): Option[PlayerPos] =
+    var nextPos = priorPos.step
+
+    // Transition to the new plane if we need to
+    if !nextPos.pos.inBounds((0, 0).pos, nextPos.plane.maxBound) then
+      // Position right before we transit between planes
+      val posBeforeTransition: PlayerPos =
+        nextPos.copy(pos = nextPos.pos.constrainToBounds((0, 0).pos, nextPos.plane.maxBound))
+      // Transition one step to cross planes
+      nextPos = part1TransitionFor(grid, priorPos.plane, priorPos.dir)(posBeforeTransition)
+
+    // Whichever plane we are on now, see if we can actually move here
+    if nextPos.plane.isWall(nextPos.pos) then
+    // Hit a wall, cannot move
+      None
+    else
+    // Move successfully to that spot
+      Some(nextPos)
+  @tailrec
+  private def applySteps(priorPos: PlayerPos, steps: Int): PlayerPos =
+    if steps <= 0 then
+      priorPos
+    else
+      // Attempt to apply the next step, stopping if we hit a wall
+      applyStep(priorPos) match
+        case Some(nextPos) => applySteps(nextPos, steps - 1)
+        case None => priorPos
+
+  def applyInstruction(priorPos: PlayerPos, instruction: Instruction): PlayerPos =
+    instruction match
+      case Instruction.Forward(steps) => applySteps(priorPos, steps)
+      case Instruction.Turn(td) => priorPos.turn(td)
 
 
-object Day22 extends SolutionWithParser[(RawGridData, List[Instruction]), Long, Long]:
+  def execute(instructions: List[Instruction]): PlayerPos =
+    instructions.foldLeft(initialPosition) { case (pos, instruction) =>
+      applyInstruction(pos, instruction)
+    }
+
+
+object Day22 extends SolutionWithParser[(PlaneGrid, List[Instruction]), Long, Long]:
   override def dayNumber: Int = 22
 
   private object Parsing:
@@ -126,7 +221,7 @@ object Day22 extends SolutionWithParser[(RawGridData, List[Instruction]), Long, 
     def rawGridLineParser: Parser[Array[Option[Boolean]]] =
       rawTileParser.rep(1).map(l => Array.from(l.toList))
 
-    def rawGridData: Parser[RawGridData] =
+    def rawGridData: Parser[PlaneGrid] =
       CommonParsers.lineSeparated(rawGridLineParser).map { lines =>
         // First let's convert our lines into a raw array extending all lines to be of equal length
         val maxLength = lines.map(_.length).max
@@ -179,15 +274,16 @@ object Day22 extends SolutionWithParser[(RawGridData, List[Instruction]), Long, 
           }
         }
 
-        RawGridData(arr)
+        PlaneGrid(arr)
       }
 
-  override def parser: Parser[(RawGridData, List[Instruction])] =
+  override def parser: Parser[(PlaneGrid, List[Instruction])] =
     CommonParsers.pair(Parsing.rawGridData, Parsing.instructionList, CommonParsers.blankLine)
 
-  override def solvePart1(input: (RawGridData, List[Instruction])): Long = ???
+  override def solvePart1(input: (PlaneGrid, List[Instruction])): Long =
+    GroveMapPt1(input(0)).execute(input(1)).score
 
-  override def solvePart2(input: (RawGridData, List[Instruction])): Long = ???
+  override def solvePart2(input: (PlaneGrid, List[Instruction])): Long = ???
 
 
 @main def run(): Unit = Day22.run()
